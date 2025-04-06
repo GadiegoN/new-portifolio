d394371d9fca7159189ea8fec7b133e6203736af
GadiegoN <ngadiego@gmail.com>
2025-04-06 11:40:44 -0300

    feat: add @vercel/analytics for enhanced analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Footer, Header } from "@/components/layout";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased flex flex-col justify-between`}
       >
+        <Analytics />
         <Header />
         <main className="max-w-5xl mx-auto px-4 py-10 bg-gradient-to-b from-[#0f172a] via-[#1e293b] to-[#0f172a]">
           {children}
